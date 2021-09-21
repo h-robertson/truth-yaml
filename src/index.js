483828a4d81caa8e3e5085aae5de0031c21bdc00
Helena <h.robertson@icloud.com>
2021-09-21 16:54:27 +0100

    Start of YAML truth file

---
+++ src/index.js
@@ -0,0 +1,18 @@
+export var data = {};
+
+
+export var state = {
+  facet_layout: "single",
+  height_mode: "auto",
+
+  font_weight: "600",
+  font_size_mode: "auto",
+  font_color: "#000000",
+  font_opacity: 1,
+  font_shadow: false
+
+};
+
+export function update() {}
+
+export function draw() {}

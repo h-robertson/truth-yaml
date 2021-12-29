7ceb73cf32cd84badf8a9bf8ecd35f447e5addd6
Helena <h.robertson@icloud.com>
2021-12-29 23:28:59 +0000

    Add more settings

--- src/index.js
+++ src/index.js
@@ -9,7 +9,29 @@ export var state = {
   font_size_mode: "auto",
   font_color: "#000000",
   font_opacity: 1,
-  font_shadow: false
+  font_shadow: false,
+
+  text_alignment: "left",
+  bar_alignment: "left",
+  column_alignment: "bottom",
+
+  padding: 0,
+  max_width: 10,
+
+  line_width: 0.2,
+  line_opacity: 1,
+  line_curve: "curveLinear",
+  line_dash_width: 2,
+  line_dash_space_width: 2,
+
+  data_point_default_size: 10,
+  data_point_min_size: 1,
+  data_point_max_size: 30,
+  responsive_dots: true,
+
+  animation_duration: 800,
+  animation_stagger: 200,
+  animate_on_load: true
 
 };
 
